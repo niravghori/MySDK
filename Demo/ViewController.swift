5669da635111815900fee0bf22cdb73c6d2e2eea
Nirav Ghori <nirav.ghori@volansystech.com>
2018-03-09 15:44:48 +0530

    podspec

---
+++ Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Nirav on 09/03/18.
+//  Copyright © 2018 Nirav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+    }
+
+
+}
+
---
+++ MySDK/MySDK.swift
@@ -0,0 +1,15 @@
+//
+//  MySDK.swift
+//  MySDK
+//
+//  Created by Nirav on 09/03/18.
+//  Copyright © 2018 Nirav. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
